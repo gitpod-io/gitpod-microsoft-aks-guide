87a18bb9608ad955d4c5e81e319923304df22d48
Simon Emms <simon@gitpod.io>
2021-11-17 12:36:49 +0000

    Update guide to use the Gitpod installer

--- setup.sh
+++ setup.sh
@@ -16,26 +16,14 @@ set -a
 SERVICES_POOL="services"
 WORKSPACES_POOL="workspaces"
 
-AKS_VERSION=${AKS_VERSION:="1.21.6"}
 K8S_NODE_VM_SIZE=${K8S_NODE_VM_SIZE:="Standard_DS3_v2"}
-GITPOD_VERSION=${GITPOD_VERSION:="0.10.0"}
+CERT_NAME="https-certificates"
+MYSQL_GITPOD_ENCRYPTION_KEY='[{"name":"general","version":1,"primary":true,"material":"4uGh1q8y2DYryJwrVMHs0kWXJlqvHWWt/KJuNi04edI="}]'
 
-function auth() {
-    AUTHPROVIDERS_CONFIG=${1:="auth-providers-patch.yaml"}
-    if [ ! -f "${AUTHPROVIDERS_CONFIG}" ]; then
-        echo "The auth provider configuration file ${AUTHPROVIDERS_CONFIG} does not exist."
-        exit 1
-    fi
-
-    login
-    setup_kubectl
-
-    echo "Using the auth providers configuration file: ${AUTHPROVIDERS_CONFIG}"
-    # Patching the configuration with the user auth provider/s
-    kubectl patch configmap auth-providers-config --type merge --patch "$(cat ${AUTHPROVIDERS_CONFIG})"
-    # Restart the server component
-    kubectl rollout restart deployment/server
-}
+# Secrets
+SECRET_DATABASE="az-sql-token"
+SECRET_REGISTRY="az-registry-token"
+SECRET_STORAGE="az-storage-token"
 
 function check_prerequisites() {
     if [ -z "${AZURE_SUBSCRIPTION_ID}" ]; then
@@ -45,16 +33,6 @@ function check_prerequisites() {
 
     if [ -z "${AZURE_TENANT_ID}" ]; then
         echo "Missing AZURE_TENANT_ID environment variable."
-        exit 1;
-    fi
-
-    if [ -z "${AZURE_CLIENT_ID}" ]; then
-        echo "Missing AZURE_CLIENT_ID environment variable."
-        exit 1;
-    fi
-
-    if [ -z "${AZURE_CLIENT_SECRET}" ]; then
-        echo "Missing AZURE_CLIENT_SECRET environment variable."
         exit 1;
     fi
 
@@ -85,12 +63,12 @@ function check_prerequisites() {
 }
 
 function install() {
+    echo "Gitpod installer version: $(gitpod-installer version | jq -r '.version')"
+
     check_prerequisites
 
     echo "Updating helm repositories..."
     helm repo add bitnami https://charts.bitnami.com/bitnami
-    helm repo add gitpod https://charts.gitpod.io
-    helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
     helm repo add jetstack https://charts.jetstack.io
     helm repo update
 
@@ -110,7 +88,15 @@ function install() {
     if [ "$(az aks show --name ${CLUSTER_NAME} --resource-group ${RESOURCE_GROUP} --query "name == '${CLUSTER_NAME}'" || echo "empty")" == "true" ]; then
       echo "Kubernetes cluster exists..."
     else
-      echo "Creating Kubernetes instance..."
+      if [ -z "${AKS_VERSION}" ]; then
+        echo "Finding Kubernetes version"
+        AKS_VERSION=$(az aks get-versions \
+          --location northeurope \
+          --query "orchestrators[?contains(orchestratorVersion, '1.21.')].orchestratorVersion" \
+          -o json | jq -r '.[-1]')
+      fi
+
+      echo "Creating Kubernetes instance with v${AKS_VERSION}..."
       az aks create \
         --enable-cluster-autoscaler \
         --enable-managed-identity \
@@ -122,7 +108,7 @@ function install() {
         --name "${CLUSTER_NAME}" \
         --node-osdisk-size "100" \
         --node-vm-size "${K8S_NODE_VM_SIZE}" \
-        --nodepool-labels "gitpod.io/workload_services=true" \
+        --nodepool-labels gitpod.io/workload_meta=true gitpod.io/workload_ide=true \
         --nodepool-name "${SERVICES_POOL}" \
         --resource-group "${RESOURCE_GROUP}" \
         --no-ssh-key \
@@ -138,7 +124,7 @@ function install() {
         --cluster-name "${CLUSTER_NAME}" \
         --enable-cluster-autoscaler \
         --kubernetes-version "${AKS_VERSION}" \
-        --labels "gitpod.io/workload_workspaces=true" \
+        --labels gitpod.io/workload_workspace_services=true gitpod.io/workload_workspace_regular=true gitpod.io/workload_workspace_headless=true \
         --max-count "50" \
         --max-pods "110" \
         --min-count "3" \
@@ -164,8 +150,14 @@ function install() {
     setup_managed_dns
     setup_mysql_database
     setup_storage
-    install_jaeger_operator
     install_gitpod
+
+    cat << EOF
+==========================
+Gitpod is now installed on your cluster
+
+Please update your DNS records with the relevant nameserver.
+EOF
 }
 
 function install_cert_manager() {
@@ -190,33 +182,32 @@ function install_cert_manager() {
 function install_gitpod() {
   echo "Installing Gitpod..."
 
-  envsubst < "${DIR}/charts/assets/gitpod-values.yaml" | helm upgrade --install gitpod gitpod/gitpod -f -
+  local CONFIG_FILE="${DIR}/gitpod-config.yaml"
 
-  echo "Create certificate..."
+  gitpod-installer init > "${CONFIG_FILE}"
 
-  cat <<EOF > gitpod-certificate.yaml
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: gitpod-certificate
-  namespace: default
-spec:
-  secretName: proxy-config-certificates
-  issuerRef:
-    name: azure-issuer
-    kind: ClusterIssuer
-  dnsNames:
-    - $DOMAIN
-    - '*.$DOMAIN'
-    - '*.ws.$DOMAIN'
-EOF
+  echo "Updating config..."
 
-  kubectl apply -f gitpod-certificate.yaml
-  rm gitpod-certificate.yaml
+  yq e -i ".certificate.name = \"${CERT_NAME}\"" "${CONFIG_FILE}"
+  yq e -i ".containerRegistry.inCluster = false" "${CONFIG_FILE}"
+  yq e -i ".containerRegistry.external.url = \"${DOCKER_REGISTRY_SERVER}\"" "${CONFIG_FILE}"
+  yq e -i ".containerRegistry.external.certificate.kind = \"secret\"" "${CONFIG_FILE}"
+  yq e -i ".containerRegistry.external.certificate.name = \"${SECRET_REGISTRY}\"" "${CONFIG_FILE}"
+  yq e -i ".database.inCluster = false" "${CONFIG_FILE}"
+  yq e -i ".database.external.certificate.kind = \"secret\"" "${CONFIG_FILE}"
+  yq e -i ".database.external.certificate.name = \"${SECRET_DATABASE}\"" "${CONFIG_FILE}"
+  yq e -i ".domain = \"${DOMAIN}\"" "${CONFIG_FILE}"
+  yq e -i ".metadata.region = \"${LOCATION}\"" "${CONFIG_FILE}"
+  yq e -i ".objectStorage.inCluster = false" "${CONFIG_FILE}"
+  yq e -i ".objectStorage.azure.credentials.kind = \"secret\"" "${CONFIG_FILE}"
+  yq e -i ".objectStorage.azure.credentials.name = \"${SECRET_STORAGE}\"" "${CONFIG_FILE}"
+  yq e -i '.workspace.runtime.containerdRuntimeDir = "/var/lib/containerd/io.containerd.runtime.v2.task/k8s.io"' "${CONFIG_FILE}"
 
-  kubectl rollout restart deployment/server
+  gitpod-installer \
+    render \
+    --config="${CONFIG_FILE}" > gitpod.yaml
 
-  echo "Gitpod successfully installed to ${DOMAIN}..."
+  kubectl apply -f gitpod.yaml
 }
 
 function install_jaeger_operator(){
@@ -240,8 +231,8 @@ function install_jaeger_operator(){
 }
 
 function login() {
-  echo "Log into Azure with Service Principal..."
-  az login --service-principal -u "${AZURE_CLIENT_ID}" -p "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}" > /dev/null 2>&1
+  echo "Log into Azure..."
+  az login
 
   echo "Set Azure subscription..."
   az account set --subscription "${AZURE_SUBSCRIPTION_ID}"
@@ -254,6 +245,7 @@ function setup_container_registry() {
     echo "Setup Azure Container Registry..."
     az acr create \
       --admin-enabled true \
+      --location "${LOCATION}" \
       --name "${REGISTRY_NAME}" \
       --resource-group "${RESOURCE_GROUP}" \
       --sku Premium
@@ -277,12 +269,13 @@ function setup_container_registry() {
     --query passwords[0].value \
     --resource-group "${RESOURCE_GROUP}")
 
-  kubectl create secret docker-registry image-builder-registry-secret \
+  echo "Create registry secret..."
+  kubectl create secret docker-registry "${SECRET_REGISTRY}" \
     --docker-server="${DOCKER_REGISTRY_SERVER}" \
     --docker-username="${DOCKER_USER}" \
     --docker-password="${DOCKER_PASSWORD}" \
     --dry-run=client -o yaml | \
-    kubectl replace -n "${namespace}" --force -f -
+    kubectl replace --force -f -
 }
 
 function setup_kubectl() {
@@ -307,16 +300,18 @@ function setup_managed_dns() {
     fi
 
     echo "Allow Kubernetes managed identity to make DNS changes..."
-    PRINCIPAL_ID=$(az aks show --name "${CLUSTER_NAME}" --resource-group "${RESOURCE_GROUP}" --query "identityProfile.kubeletidentity.objectId" -o tsv)
     ZONE_ID=$(az network dns zone show --name "${DOMAIN}" --resource-group "${RESOURCE_GROUP}" --query "id" -o tsv)
 
-    # The Service Principal needs "Group Administrator" role
-    # @link https://www.simonemms.com/blog/2021/01/10/setting-terraform-service-principal-to-work-with-azure-active-directory
+    KUBELET_OBJECT_ID=$(az aks show --name "${CLUSTER_NAME}" --resource-group "${RESOURCE_GROUP}" --query "identityProfile.kubeletidentity.objectId" -o tsv)
+    KUBELET_CLIENT_ID=$(az aks show --name "${CLUSTER_NAME}" --resource-group "${RESOURCE_GROUP}" --query "identityProfile.kubeletidentity.clientId" -o tsv)
+
     az role assignment create \
-      --assignee "${PRINCIPAL_ID}" \
+      --assignee "${KUBELET_OBJECT_ID}" \
       --role "DNS Zone Contributor" \
       --scope "${ZONE_ID}"
 
+    # Use v5.4.8 as external-dns v0.10.x has issue using Azure managed identities not in v0.9.0
+    # @link https://github.com/kubernetes-sigs/external-dns/issues/2383
     helm upgrade \
       --atomic \
       --cleanup-on-fail \
@@ -326,11 +321,12 @@ function setup_managed_dns() {
       --reset-values \
       --set provider=azure \
       --set azure.resourceGroup="${RESOURCE_GROUP}" \
-      --set azure.aadClientId="${AZURE_CLIENT_ID}" \
-      --set azure.aadClientSecret="${AZURE_CLIENT_SECRET}" \
       --set azure.subscriptionId="${AZURE_SUBSCRIPTION_ID}" \
       --set azure.tenantId="${AZURE_TENANT_ID}" \
+      --set azure.useManagedIdentityExtension=true \
+      --set azure.userAssignedIdentityID="${KUBELET_CLIENT_ID}" \
       --set logFormat=json \
+      --version=5.4.8 \
       --wait \
       external-dns \
       bitnami/external-dns
@@ -340,55 +336,60 @@ function setup_managed_dns() {
   fi
 }
 
-# @todo allow Gitpod to work with external Azure DB https://github.com/gitpod-io/gitpod/issues/5508
 function setup_mysql_database() {
-  export MYSQL_GITPOD_PASSWORD=$(openssl rand -base64 20)
+  MYSQL_GITPOD_USERNAME="gitpod"
+  MYSQL_GITPOD_PASSWORD=$(openssl rand -base64 20)
 
-  # if [ "$(az mysql server show --name ${MYSQL_INSTANCE_NAME} --resource-group ${RESOURCE_GROUP} --query "name == '${MYSQL_INSTANCE_NAME}'" || echo "empty")" == "true" ]; then
-  #   echo "MySQL instance exists - updating password..."
-  #   az mysql server update \
-  #     --admin-password "${MYSQL_GITPOD_PASSWORD}" \
-  #     --name "${MYSQL_INSTANCE_NAME}" \
-  #     --resource-group "${RESOURCE_GROUP}"
-  # else
-  #   echo "Creating MySQL instance..."
-  #   az mysql server create \
-  #     --admin-user gitpod \
-  #     --admin-password "${MYSQL_GITPOD_PASSWORD}" \
-  #     --auto-grow Enabled \
-  #     --name "${MYSQL_INSTANCE_NAME}" \
-  #     --public Enabled \
-  #     --resource-group "${RESOURCE_GROUP}" \
-  #     --sku-name GP_Gen5_2 \
-  #     --ssl-enforcement Disabled \
-  #     --storage-size 20480 \
-  #     --version "5.7"
+  if [ "$(az mysql server show --name ${MYSQL_INSTANCE_NAME} --resource-group ${RESOURCE_GROUP} --query "name == '${MYSQL_INSTANCE_NAME}'" || echo "empty")" == "true" ]; then
+    echo "MySQL instance exists - updating password..."
+    az mysql server update \
+      --admin-password "${MYSQL_GITPOD_PASSWORD}" \
+      --name "${MYSQL_INSTANCE_NAME}" \
+      --resource-group "${RESOURCE_GROUP}"
+  else
+    echo "Creating MySQL instance..."
+    az mysql server create \
+      --admin-user "${MYSQL_GITPOD_USERNAME}" \
+      --admin-password "${MYSQL_GITPOD_PASSWORD}" \
+      --auto-grow Enabled \
+      --location "${LOCATION}" \
+      --name "${MYSQL_INSTANCE_NAME}" \
+      --public Enabled \
+      --resource-group "${RESOURCE_GROUP}" \
+      --sku-name GP_Gen5_2 \
+      --ssl-enforcement Disabled \
+      --storage-size 20480 \
+      --version "5.7"
+  fi
 
-  #   echo "Creating MySQL replica..."
-  #   az mysql server replica create \
-  #     --name "${MYSQL_INSTANCE_NAME}-replica" \
-  #     --source-server "${MYSQL_INSTANCE_NAME}" \
-  #     --resource-group "${RESOURCE_GROUP}"
-  # fi
+  DB_NAME=gitpod
+  if [ "$(az mysql db show --name ${DB_NAME} --resource-group ${RESOURCE_GROUP} --server-name ${MYSQL_INSTANCE_NAME} --query "name == '${DB_NAME}'" || echo "empty")" == "true" ]; then
+    echo "Gitpod MySQL database exists..."
+  else
+    echo "Creating Gitpod MySQL database..."
+    az mysql db create \
+      --name "${DB_NAME}" \
+      --resource-group "${RESOURCE_GROUP}" \
+      --server-name "${MYSQL_INSTANCE_NAME}"
+  fi
 
-  # DB_NAME=gitpod
-  # if [ "$(az mysql db show --name ${DB_NAME} --resource-group ${RESOURCE_GROUP} --server-name ${MYSQL_INSTANCE_NAME} --query "name == '${DB_NAME}'" || echo "empty")" == "true" ]; then
-  #   echo "Gitpod MySQL database exists..."
-  # else
-  #   echo "Creating Gitpod MySQL database..."
-  #   az mysql db create \
-  #     --name "${DB_NAME}" \
-  #     --resource-group "${RESOURCE_GROUP}" \
-  #     --server-name "${MYSQL_INSTANCE_NAME}"
-  # fi
+  echo "Allow Azure resources to access MySQL database..."
+  az mysql server firewall-rule create \
+    --end-ip-address "0.0.0.0" \
+    --name "Azure_Resources" \
+    --resource-group "${RESOURCE_GROUP}" \
+    --server-name "${MYSQL_INSTANCE_NAME}" \
+    --start-ip-address "0.0.0.0"
 
-  # echo "Allow Azure resources to access MySQL database..."
-  # az mysql server firewall-rule create \
-  #   --end-ip-address "0.0.0.0" \
-  #   --name "Azure_Resources" \
-  #   --resource-group "${RESOURCE_GROUP}" \
-  #   --server-name "${MYSQL_INSTANCE_NAME}" \
-  #   --start-ip-address "0.0.0.0"
+  echo "Create database secret..."
+  kubectl create secret generic "${SECRET_DATABASE}" \
+    --from-literal=encryptionKeys="${MYSQL_GITPOD_ENCRYPTION_KEY}" \
+    --from-literal=host="${MYSQL_INSTANCE_NAME}.mysql.database.azure.com" \
+    --from-literal=password="${MYSQL_GITPOD_PASSWORD}" \
+    --from-literal=port="3306" \
+    --from-literal=username="${MYSQL_GITPOD_USERNAME}@${MYSQL_INSTANCE_NAME}" \
+    --dry-run=client -o yaml | \
+    kubectl replace --force -f -
 }
 
 function setup_storage() {
@@ -399,6 +400,7 @@ function setup_storage() {
     az storage account create \
       --access-tier Hot \
       --kind StorageV2 \
+      --location "${LOCATION}" \
       --name "${STORAGE_ACCOUNT_NAME}" \
       --resource-group "${RESOURCE_GROUP}" \
       --sku Standard_LRS
@@ -417,11 +419,18 @@ function setup_storage() {
     --role "Storage Blob Data Contributor" \
     --scope "${STORAGE_ACCOUNT_ID}"
 
-  export STORAGE_ACCOUNT_KEY=$(az storage account keys list \
+  STORAGE_ACCOUNT_KEY=$(az storage account keys list \
       --account-name "${STORAGE_ACCOUNT_NAME}" \
       --resource-group "${RESOURCE_GROUP}" \
       --output json \
         | jq -r '.[] | select(.keyName == "key1") | .value')
+
+  echo "Create storage secret..."
+  kubectl create secret generic "${SECRET_STORAGE}" \
+      --from-literal=accountName="${STORAGE_ACCOUNT_NAME}" \
+      --from-literal=accountKey="${STORAGE_ACCOUNT_KEY}" \
+      --dry-run=client -o yaml | \
+      kubectl replace --force -f -
 }
 
 function uninstall() {
@@ -455,9 +464,6 @@ function uninstall() {
 
 function main() {
   case $1 in
-    '--auth')
-      auth "auth-providers-patch.yaml"
-    ;;
     '--install')
       install
     ;;
@@ -466,7 +472,7 @@ function main() {
     ;;
     *)
       echo "Unknown command: $1"
-      echo "Usage: $0 [--install|--uninstall|--auth]"
+      echo "Usage: $0 [--install|--uninstall]"
     ;;
   esac
   echo "Done"
